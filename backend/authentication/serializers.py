b6c16f04639850813b0e828d3397537ad542d8e9
Milos Miljanic <milosmilos96@gmail.com>
2018-04-20 01:34:00 +0200

    remove password from serialization

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -5,7 +5,6 @@ from .models import User, ROLES
 class UserSerializer(serializers.Serializer):
   id = serializers.IntegerField(read_only=True)
   username = serializers.CharField(required=True, allow_blank=False, max_length=30)
-  password = serializers.CharField(required=True, allow_blank=False, max_length=255)
   first_name = serializers.CharField(required=False, allow_blank=True, max_length=30)
   last_name = serializers.CharField(required=False, allow_blank=True, max_length=30)
   role = serializers.ChoiceField(choices=ROLES, default='user')
