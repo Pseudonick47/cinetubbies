8c7d4a0f1df9c68dfba45a2bdc0d8ca9cb1b1891
Milos Miljanic <milosmilos96@gmail.com>
2018-04-23 18:47:52 +0200

    add sending mail on register

--- backend/authentication/utils/auth.py
+++ backend/authentication/utils/auth.py
@@ -1,7 +1,33 @@
 from authentication.serializers import UserSerializer
+from django.core.mail import send_mail
 
 def jwt_response_payload_handler(token, user=None, request=None):
   return {
     'token': token,
     'user': UserSerializer(user, context={'request': request}).data
   }
+
+def send_email(mail_subject, mail_from, mail_to, mail_message):
+  try:
+    send_mail(
+      mail_subject,
+      mail_message,
+      mail_from,
+      [mail_to],
+      fail_silently=False,
+    )
+  except Exception:
+    pass
+
+def send_verification_mail(user, token):
+  message = f"""
+    Hello, welcome to Cinetubbies
+    Please go to link to verify the email
+    ovo.je/neki/link/{token}
+  """
+  send_email(
+    'Verify Your Account',
+    'tinkivinki@cinetubbies.com',
+    user.email,
+    message
+  )
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -31,6 +31,7 @@ class UserViewSet(viewsets.ModelViewSet):
     jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
     payload = jwt_payload_handler(user)
     token = jwt_encode_handler(payload)
+    auth.send_verification_mail(user, 'token')
     return Response(auth.jwt_response_payload_handler(token, user))
 
   def partial_update(self, request, pk=None):
